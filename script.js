2364b7f4639e378134169006bb624a08404589df
sujeet kumar <sujeetkr2508@gmail.com>
2023-03-15 16:19:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,196 @@
+// SELECT ALL ELEMENTS
+const countryNameEl = document.querySelector(".country .name");
+const totalCasesEl = document.querySelector(".total-cases .value");
+const newCasesEl = document.querySelector(".total-cases .new-value");
+const recoveredEl = document.querySelector(".recovered .value");
+const newRecoveredEl = document.querySelector(".recovered .new-value");
+const deathEl = document.querySelector(".deaths .value");
+const newDeathsEl = document.querySelector(".deaths .new-value");
+
+const chartEl = document.getElementById("axes_line_chart").getContext("2d");
+
+// APP VARIABLES
+let app_data = [],
+  casesList = [],
+  recoveredList = [],
+  deaths_list = [],
+  deaths = [],
+  formatedDates = [];
+
+// GET USERS COUNTRY CODE
+fetch("https://api.ipgeolocation.io/ipgeo?apiKey=14c7928d2aef416287e034ee91cd360d")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    let country_code = data.country_code2;
+    let user_country;
+    country_list.forEach((country) => {
+      if (country.code == country_code) {
+        user_country = country.name;
+      }
+    });
+    fetchData(user_country);
+  });
+
+/* ---------------------------------------------- */
+/*                     FETCH API                  */
+/* ---------------------------------------------- */
+function fetchData(country) {
+  user_country = country;
+  countryNameEl.innerHTML = "Loading...";
+
+  (casesList = []),
+    (recoveredList = []),
+    (deaths_list = []),
+    (dates = []),
+    (formatedDates = []);
+
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  const api_fetch = async (country) => {
+    await fetch(
+      "https://api.covid19api.com/total/country/" + country + "/status/confirmed",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          dates.push(entry.Date);
+          casesList.push(entry.Cases);
+        });
+      });
+
+    await fetch(
+      "https://api.covid19api.com/total/country/" + country + "/status/recovered",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          recoveredList.push(entry.Cases);
+        });
+      });
+
+    await fetch(
+      "https://api.covid19api.com/total/country/" + country + "/status/deaths",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          deaths_list.push(entry.Cases);
+        });
+      });
+
+    updateUI();
+  };
+
+  api_fetch(country);
+}
+
+// UPDATE UI FUNCTION
+function updateUI() {
+  updateStats();
+  axesLinearChart();
+}
+
+function updateStats() {
+  const total_cases = casesList[casesList.length - 1];
+  const new_confirmed_cases = total_cases - casesList[casesList.length - 2];
+
+  const total_recovered = recoveredList[recoveredList.length - 1];
+  const new_recovered_cases = total_recovered - recoveredList[recoveredList.length - 2];
+
+  const total_deaths = deaths_list[deaths_list.length - 1];
+  const new_deaths_cases = total_deaths - deaths_list[deaths_list.length - 2];
+
+  countryNameEl.innerHTML = user_country;
+  totalCasesEl.innerHTML = total_cases;
+  newCasesEl.innerHTML = `+${new_confirmed_cases}`;
+  recovered_element.innerHTML = total_recovered;
+  newRecoveredEl.innerHTML = `+${new_recovered_cases}`;
+  deathEl.innerHTML = total_deaths;
+  newDeathsEl.innerHTML = `+${new_deaths_cases}`;
+
+  // format dates
+  dates.forEach((date) => {
+    formatedDates.push(formatDate(date));
+  });
+}
+
+// UPDATE CHART
+let my_chart;
+function axesLinearChart() {
+  if (my_chart) {
+    my_chart.destroy();
+  }
+
+  my_chart = new Chart(chartEl, {
+    type: "line",
+    data: {
+      datasets: [
+        {
+          label: "Cases",
+          data: casesList,
+          fill: false,
+          borderColor: "#FFF",
+          backgroundColor: "#FFF",
+          borderWidth: 1,
+        },
+        {
+          label: "Recovered",
+          data: recoveredList,
+          fill: false,
+          borderColor: "#009688",
+          backgroundColor: "#009688",
+          borderWidth: 1,
+        },
+        {
+          label: "Deaths",
+          data: deaths_list,
+          fill: false,
+          borderColor: "#f44336",
+          backgroundColor: "#f44336",
+          borderWidth: 1,
+        },
+      ],
+      labels: formatedDates,
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  });
+}
+
+// FORMAT DATES
+const monthsNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+function formatDate(dateString) {
+  let date = new Date(dateString);
+
+  return `${date.getDate()} ${monthsNames[date.getMonth()]}`;
+}
